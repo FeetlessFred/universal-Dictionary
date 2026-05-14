cbde7f76dcf7f1ec4140f6e84884fd9616084bbc
FeetlessFred <S0144652@terrace.qld.edu.au>
2026-05-15 08:10:11 +1000

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,5 @@
 async function loadPosts() {
-  const res = await fetch("http://localhost:5000/api/posts");
+  const res = await fetch("https://terrace-tube.onrender.com/api/posts");
   const posts = await res.json();
 
   const feed = document.getElementById("feed");
@@ -8,7 +8,7 @@ async function loadPosts() {
     feed.innerHTML += `
       <div class="post">
         <h2>${post.username}</h2>
-        <img src="http://localhost:5000/${post.image}">
+        <img src="https://terrace-tube.onrender.com/${post.image}">
         <p>${post.caption}</p>
       </div>
     `;
