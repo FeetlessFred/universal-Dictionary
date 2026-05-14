579fadd0e3f37c3d614e0c92ad812fcca9366536
FeetlessFred <S0144652@terrace.qld.edu.au>
2026-05-14 18:32:50 +1000

    Create users.js

---
+++ backend/routes/users.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+const User = require("../models/User");
+
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const updated = await User.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    res.json(updated);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
